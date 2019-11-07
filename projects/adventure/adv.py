df5e49e982dea2e08e813e80b439d8c81b07c91c
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-11-07 10:21:45 +0100

    Micro Polya

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -25,6 +25,20 @@ player = Player("Name", world.startingRoom)
 traversalPath = []
 
 
+# Function that fills traversalpath with directions that, when walked in order, will visit every room on the map at least once
+
+# Probably need a combination of BFS & DFS
+
+# Create a loop/process that:
+    # - Tries moving in a particular direction
+    # - If you find yourself in a new room, update the entries on your graph (showing that the previous room has an exit that leads to the current room and vice versa)
+
+    # Base case is when there are exactly 500 entries in the graph & no `'?'` in the adjacency dictionairies
+
+# Do a BFS to find the nearest room with `?` for an exit.
+# Then, do a DFS on everything past the '?' until it runs into a case where there are no more '?'. Adding the rooms to traversalPath all the while.
+    # In that case, move back up the stack until there is one.
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
