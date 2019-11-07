36617604ce34edf3a83ce7ef799ea0d5eafaf6ae
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-11-07 10:53:51 +0100

    Setup for DFT

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,29 +1,16 @@
 from room import Room
 from player import Player
 from world import World
-from room_storage import roomGraph
+from room_storage import roomGraph1 as roomGraph
 
-import random
 
 # Load world
 world = World()
-
-
-
 world.loadGraph(roomGraph)
-
-# UNCOMMENT TO VIEW MAP
 world.printRooms()
-
 player = Player("Name", world.startingRoom)
 
-# Fill this out
-traversalPath = []
 
-
-# Function that fills traversalpath with directions that, when walked in order, will visit every room on the map at least once
-
-# Probably need a combination of BFS & DFS
 
 # Create a loop/process that:
     # - Tries moving in a particular direction
@@ -35,8 +22,54 @@ traversalPath = []
 # Then, do a DFS on everything past the '?' until it runs into a case where there are no more '?'. Adding the rooms to traversalPath all the while.
     # In that case, move back up the stack until there is one.
 
-# Create traversal path:
-# BFS out from world.startingRoom
+
+
+
+# start at player.currentRoom
+
+# run DFT until you hit dead end (room with no unvisited neighbours) OR you have visited every room
+
+# on every move, add room to traversalPath
+# mark every visited room as visited
+
+# if you find a room with no more unvisited neighbours (dead end)
+
+# run BFT to find SHORTEST PATH to first room that is unvisited
+# on every move to that room save path
+# then run this path with the player so you get to that room
+
+# continute DFT loop (until you find another dead end, then repeat BFT again)
+
+# >>> Create separate functions for DFT and BFT
+
+def dft(player):
+    visited = []
+    stack = []
+    stack.append(player.currentRoom)
+
+    while len(stack) > 0:
+        # run DFT
+        # Remove item from stack
+        # Evaluate if we've already visited current room
+            # If not, add current_room to visited
+
+            # Track stack size before evaluating rooms and exits
+
+            # Declare a neighbouring rooms dict, which we'll use to store the exits in the neighbouring rooms to evaluate possible dead-ends.
+            # neighbouring_rooms = {'n': None, 'w': None, 's': None, 'e': None}
+
+            # for room in current_room.getExits():
+                # room = current_room.getRoomInDirection(direction)
+                # neighbouring_rooms[direction] = room
+
+        # else reloop
+        pass
+    pass
+
+def bft():
+    pass
+
+print(player.currentRoom.getRoomInDirection('n'))
 
 
 
@@ -48,6 +81,11 @@ traversalPath = []
 
 
 
+
+traversalPath = dft(player)
+
+print(f'path: {traversalPath}')
+print(f'path length: {len(traversalPath)}')
 
 
 
